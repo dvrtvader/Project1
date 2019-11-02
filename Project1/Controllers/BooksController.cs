f8b4ef46785fde185a46f7c22ac9216d75c86bca
Aidyn Shayahmetov <aidoka-98@mail.ru>
2019-11-02 10:04:10 +0600

    Initialize project

---
+++ Project1/Controllers/BooksController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project1.Data;
+using Project1.Models.Books;
+
+namespace Project1.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BooksContext _dbContext;
+
+        public BooksController(BooksContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _dbContext.Books.ToListAsync();
+            return View(books);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Book book)
+        {
+
+
+            _dbContext.Books.Add(book);
+            await _dbContext.SaveChangesAsync();
+
+            var books = await _dbContext.Books.ToListAsync();
+
+            return View("Index", books);
+        }
+
+        public async Task<IActionResult> Search(string text)
+        {
+            text = text.ToLower();
+            var searchedBooks = await _dbContext.Books.Where(book => book.Name.ToLower().Contains(text)
+                                            || book.Genre.ToLower().Contains(text)
+                                            || book.Author.ToLower().Contains(text))
+                                        .ToListAsync();
+            return View("Index", searchedBooks);
+        }
+
+    }
+}
---
+++ Project1/Migrations/20191101190303_dataSeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project1.Migrations
+{
+    public partial class dataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
